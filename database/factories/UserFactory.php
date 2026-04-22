929a242cdec4f0a6ffa878cf759713a81d7f14f9
Trebes Stan <stan.trebes@etu.unistra.fr>
2026-04-22 11:40:25 +0200

    Modification du seeder User pour ajouter le prénom

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -25,6 +25,7 @@ class UserFactory extends Factory
     public function definition(): array
     {
         return [
+            'firstname' => fake()->firstName(),
             'name' => fake()->name(),
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
