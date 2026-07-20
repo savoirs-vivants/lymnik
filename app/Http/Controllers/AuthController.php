efb05fd5b5e5bde305488afd8895d3513aa119fc
Trebes Stan <stan.trebes@etu.unistra.fr>
2026-07-20 17:02:15 +0200

    Assigne le role participant a l inscription depuis la page register

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -60,7 +60,7 @@ class AuthController extends Controller
             'name'       => $request->name,
             'email'      => $request->email,
             'password'   => Hash::make($request->password),
-            'role'       => 'user',
+            'role'       => 'participant',
         ]);
 
         Auth::login($user);
