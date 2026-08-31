157ac27db600dd7dd3b19d3aa39e287832b198d7
Trebes Stan <stan.trebes@etu.unistra.fr>
2026-08-31 17:43:22 +0200

    Modification lien map

--- resources/js/core/map-utils.js
+++ resources/js/core/map-utils.js
@@ -19,9 +19,10 @@ export function createBaseMap(
     });
 
     L.tileLayer(
-        "https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png",
+        "https://tile.openstreetmap.org/{z}/{x}/{y}.png",
         {
             maxZoom: 19,
+            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
         },
     ).addTo(map);
 
